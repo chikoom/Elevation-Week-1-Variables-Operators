02e24f67143cf1fe0a5062de88a9ed26d404ede1
Chikoom <callforcopy@gmail.com>
2020-06-17 21:10:13 +0300

    Project commit and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Elevation Academy - Variables & Operators exercise
+
+![JS LOGO](https://res.cloudinary.com/chikoom/image/upload/v1592417295/GitHub%20READMEs/js_apowps.jpg)
+  &nbsp;
+  &nbsp;
+  ## Simple JS exercise
+
+
---
+++ main.js
@@ -0,0 +1,45 @@
+// Exercise 1
+exDivider(1);
+
+const maxCapacity = 14;
+let surfTime = true;
+let bestStudent;
+//const purposeInLife;
+
+console.log(maxCapacity);
+console.log(surfTime);
+console.log(bestStudent);
+//console.log(purposeInLife);
+
+// Exercise 2
+exDivider(2);
+
+const myVariable = "The potential of the future ";
+console.log(myVariable + "is the best thing ever");
+
+// Exercise 3
+exDivider(3);
+const password = 1234;
+const confirmPassword = 1234;
+
+if (password === confirmPassword) {
+  console.log("passwords match");
+} else {
+  console.log("passwords did not match");
+}
+
+// Exercise 4
+exDivider(4);
+
+console.log(423 * 12);
+console.log(802 / 2);
+console.log((5 + 6) * 3);
+
+function exDivider(number) {
+  console.log(" ");
+  console.log(" ");
+  console.log("==========================");
+  console.log("Exercise " + number);
+  console.log("==========================");
+  console.log(" ");
+}
